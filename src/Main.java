97751c59ca5297a8059627599ea7060b542aa1f0
hamidie ahmat <ahmathisseinh@gmail.com>
2024-10-18 20:14:27 +0100

    Değişkenler ve Veri Tipler

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int width=10;
+        int length =13;
+        int areas=length*width;
+        int perimeter =2*(length+width);
+        System.out.println("the area of our rectangle is:"+areas );
+        System.out.println("the perimeter  of our rectangle is:"+perimeter);
+
     }
 }
