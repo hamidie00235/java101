b6785ce5da38fbd07ecbbb66c9b4897e5f7ae717
hamidie ahmat <ahmathisseinh@gmail.com>
2024-10-18 23:02:39 +0100

    Byte, Short, Int ve Long Veri Tipleri

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        int width=10;
-        int length =13;
-        int areas=length*width;
-        int perimeter =2*(length+width);
-        System.out.println("the area of our rectangle is:"+areas );
-        System.out.println("the perimeter  of our rectangle is:"+perimeter);
+//        int width=10;
+//        int length =13;
+//        int areas=length*width;
+//        int perimeter =2*(length+width);
+//        System.out.println("the area of our rectangle is:"+areas );
+//        System.out.println("the perimeter  of our rectangle is:"+perimeter);
+
+        // tam sayilari byte,long int and short;
+        byte vByte =100;
+        short vShort =1000;
+        int vInt=10000;
+        long vLong =10000000;
+        System.out.println(vByte);
+        System.out.println(vShort);
+        System.out.println(vInt);
+        System.out.println(vLong);
 
     }
 }
