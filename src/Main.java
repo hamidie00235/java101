87322156d6f7b247d48c95a79a1bf2ed181e9b00
hamidie ahmat <ahmathisseinh@gmail.com>
2024-10-23 12:57:19 +0100

    Char ve Boolean Veri Tipleri

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,30 @@ public class Main {
 //        System.out.println("the perimeter  of our rectangle is:"+perimeter);
 
         // tam sayilari byte,long int and short;
-        byte vByte =100;
-        short vShort =1000;
-        int vInt=10000;
-        long vLong =10000000;
-        System.out.println(vByte);
-        System.out.println(vShort);
-        System.out.println(vInt);
-        System.out.println(vLong);
+//        byte vByte =100;
+//        short vShort =1000;
+//        int vInt=10000;
+//        long vLong =10000000;
+//        System.out.println(vByte);
+//        System.out.println(vShort);
+//        System.out.println(vInt);
+//        System.out.println(vLong);
+
+       // Char ve Boolean Veri Tipleri
+
+      // char
+        char c1='j';
+        char c2='a';
+        char c3='v';
+        char c4='a';
+        System.out.println(""+c1+c2+c3+c4);
+
+        // boolean
+        boolean vrai=true;
+        boolean  faux=false;
+        System.out.println(vrai);
 
     }
+
+
 }
