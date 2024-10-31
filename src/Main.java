172140308916fc434213ae88bb9ab140619888fd
hamidie ahmat <ahmathisseinh@gmail.com>
2024-10-31 10:59:59 +0100

    Ödev: Girilen 3 sayıyı "küçükten büyüğe" sıralayan programı

--- src/Main.java
+++ src/Main.java
@@ -538,27 +538,27 @@ Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.
         System.out.println("Enter the value of c:");
         c= input.nextInt();
 
-        if(a>b && a>c){
-            if(b>c){
-                System.out.println("a>b>c");
+        if(a<b && a<c){
+            if(b<c){
+                System.out.println("a<b<c");
             }
             else {
-                System.out.println("a>c>b");
+                System.out.println("a<c<b");
             }
-        } else if (b>a && b>c) {
-            if(a>c){
-                System.out.println("b>a>c");
+        } else if (b<a && b<c) {
+            if(a<c){
+                System.out.println("b<a<c");
             }
             else {
-                System.out.println("b>c>a");
+                System.out.println("b<c<a");
             }
 
-        } else if (c>b && c>a) {
-            if(a>b){
-                System.out.println("c>a>b");
+        } else if (c<b && c<a) {
+            if(a<b){
+                System.out.println("c<a<b");
             }
             else {
-                System.out.println("c>b>a");
+                System.out.println("c<b<a");
             }
 
         }
